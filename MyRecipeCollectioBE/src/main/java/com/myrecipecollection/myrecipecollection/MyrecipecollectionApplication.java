50649b5cd6353c01c6a9d907a0c005774664a314
Tibi <bukkfabiszti@gmail.com>
2020-01-07 20:16:48 +0100

    added both projects

---
+++ MyRecipeCollectioBE/src/main/java/com/myrecipecollection/myrecipecollection/MyrecipecollectionApplication.java
@@ -0,0 +1,13 @@
+package com.myrecipecollection.myrecipecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecipecollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyrecipecollectionApplication.class, args);
+    }
+
+}
---
+++ MyRecipeCollectioBE/src/test/java/com/myrecipecollection/myrecipecollection/MyrecipecollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrecipecollection.myrecipecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrecipecollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
