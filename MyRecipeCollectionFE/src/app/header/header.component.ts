1e08faf5964803b391a204476a8a39aca020a16e
Tibi <bukkfabiszti@gmail.com>
2020-01-07 21:50:11 +0100

    added fontawesome icon, added animation to icon

--- MyRecipeCollectionFE/src/app/header/header.component.ts
+++ MyRecipeCollectionFE/src/app/header/header.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
+  sideNavOpen = false;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
